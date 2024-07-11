17ff711d44289865c80afba1aa819171836e12a8
Ibrahim <mamansasni.ibrahim01@gmail.com>
2024-07-11 09:26:35 -0700

    update1-country_users

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,3 +1,3 @@
 -- Write a SQL script that creates a table users
 -- If the table already exists, your script should not fail
-CREATE TABLE IF NOT EXISTS users(id INT NOT NULL AUTO_INCREMENT,PRIMARY KEY(id), email CHAR(255) NOT NULL UNIQUE, name CHAR(255), country ENUM('US', 'CO', 'TU') NOT NULL);
+CREATE TABLE IF NOT EXISTS users(id INT NOT NULL AUTO_INCREMENT,PRIMARY KEY(id), email CHAR(255) NOT NULL UNIQUE, name CHAR(255), country ENUM('US', 'CO', 'TU') NOT NULL DEFAULT 'US');
