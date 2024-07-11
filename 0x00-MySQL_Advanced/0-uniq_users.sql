e013baafc59a05ea63f6a14020e1cb979e350d37
Ibrahim <mamansasni.ibrahim01@gmail.com>
2024-07-11 09:17:02 -0700

    update 0-uniq_users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,3 @@
 -- Write a SQL script that creates a table users
 -- If the table already exists, your script should not fail
-CREATE TABLE IF NOT EXISTS users(id INT NOT NULL, email CHAR(255) NOT NULL UNIQUE, name CHAR(255));
+CREATE TABLE IF NOT EXISTS users(id INT NOT NULL AUTO_INCREMENT,PRIMARY KEY(id), email CHAR(255) NOT NULL UNIQUE, name CHAR(255));
