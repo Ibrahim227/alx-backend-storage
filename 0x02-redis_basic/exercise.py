0a2b1a18cd36fd93d5a508d47d56bc55f17e2125
Ibrahim <mamansani.ibrahim@gmail.com>
2024-07-17 15:30:40 -0700

    update exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 """Import requried module/lib"""
+import random
+import redis
+import uuid
 
+
+class Cache:
+    """Create a Cache class."""
+    def __init__(self, _redis):
+        """Initialize the class"""
+        self._redis = redis.Redis()
+
+    def store(data) -> str:
+        """return a string"""
+        data = random(str(uuid.uuid()))
+        return str(data)
